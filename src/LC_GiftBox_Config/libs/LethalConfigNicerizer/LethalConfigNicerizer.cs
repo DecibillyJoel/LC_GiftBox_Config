43dca77c76b14afe3f0d4c535b9570d0ae3d4b44
DBJ <23534814+DecibillyJoel@users.noreply.github.com>
2024-11-12 03:51:01 -0500

    Go

--- src/LC_GiftBox_Config/libs/LethalConfigNicerizer/LethalConfigNicerizer.cs
+++ src/LC_GiftBox_Config/libs/LethalConfigNicerizer/LethalConfigNicerizer.cs
@@ -4,6 +4,7 @@ using System.Runtime.CompilerServices;
 using BepInEx.Bootstrap;
 using BepInEx.Configuration;
 using LethalConfig;
+using MonoMod.Utils;
 
 namespace LC_GiftBox_Config.libs.LethalConfigNicerizer;
 
@@ -11,32 +12,48 @@ internal static class LethalConfigNicerizer
 {
     internal const string LethalConfig_GUID = "ainavt.lc.lethalconfig";
 
-    internal static bool IsActive {
+	internal static bool CanHasNicerizationPlease {
         get => Chainloader.PluginInfos.ContainsKey(LethalConfig_GUID);
     }
 
 	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-    internal static void AddConfigItem(ConfigEntry<int> entry, bool isSlider = true, bool restartRequired = false)
+	internal static ConfigEntry<T> Nicerize<T>(ConfigEntry<T> entry, bool restartRequired = false)
+	{
+		if (entry is ConfigEntry<int>) {
+			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<int>)!, restartRequired);
+		} else if (entry is ConfigEntry<float>) {
+			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<float>)!, restartRequired);
+		} else if (entry is ConfigEntry<bool>) {
+			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<bool>)!, restartRequired);
+		} else if (entry is ConfigEntry<string>) {
+			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<string>)!, restartRequired);
+		} else if (entry is ConfigEntry<Enum>) {
+			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<Enum>)!, restartRequired);
+		} else {
+			throw new ArgumentException($"[libs.LethalConfigNicerizer.Nicerize] Cannot Nicerize ConfigEntry<{typeof(T)}>!");
+		}
+
+		return entry;
+	}
+
+	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+    internal static void AddConfigItem(ConfigEntry<int> entry, bool restartRequired = false)
     {
-		if(isSlider) 
-		{
-        	LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.IntSliderConfigItem(entry, restartRequired));
+		if(entry.Description.AcceptableValues != null) {
+			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.IntSliderConfigItem(entry, restartRequired));
 		} 
-		else 
-		{
+		else {
 			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.IntInputFieldConfigItem(entry, restartRequired));
 		}
     }
 
 	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-    internal static void AddConfigItem(ConfigEntry<float> entry, bool isSlider = true, bool restartRequired = false)
+    internal static void AddConfigItem(ConfigEntry<float> entry, bool restartRequired = false)
     {
-		if(isSlider) 
-		{
-        	LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.FloatSliderConfigItem(entry, restartRequired));
+		if(entry.Description.AcceptableValues != null) {
+			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.FloatSliderConfigItem(entry, restartRequired));
 		} 
-		else 
-		{
+		else {
 			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.FloatInputFieldConfigItem(entry, restartRequired));
 		}
     }
@@ -48,10 +65,9 @@ internal static class LethalConfigNicerizer
     }
 
 	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-    internal static void AddConfigItem(ConfigEntry<string> entry, bool isDropdown = false, bool restartRequired = false)
+    internal static void AddConfigItem(ConfigEntry<string> entry, bool restartRequired = false)
     {
-		if (isDropdown)
-		{
+		if(entry.Description.AcceptableValues != null) {
 			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.TextDropDownConfigItem(entry, restartRequired));
 		}
 		else
